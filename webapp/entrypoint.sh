bb806ca1588e395118bc088b4dcb61a06ede539e
Leonel Dias <leonel.j.dias@gmail.com>
2025-05-26 17:21:39 +0100

    feat: Set up Docker environment with Nginx, PostgreSQL, and Supervisor; configure Vite for development

---
+++ webapp/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+echo "Starting entrypoint script..."
+
+# Check if the .env file exists
+if [ ! -f .env ]; then
+    echo ".env file not found. Copying from .env.example..."
+    cp .env.example .env
+else
+    echo ".env file already exists."
+fi
+
+# Print the current environment
+echo "Current environment: $APP_ENV"
+
+# Install dependencies
+composer install --no-interaction --no-dev --optimize-autoloader
+npm install --no-audit --no-fund --prefer-dist
+
+# Clear configurations to avoid caching issues in development
+echo "Clearing configurations..."
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+# Run migrations
+php artisan migrate --force
+
+# Build vite assets
+if [ "$APP_ENV" = "production" ]; then
+    echo "Building Vite assets for production..."
+    npm run build
+fi
+
+# Start Supervisord to manage processes
+echo "Supervisord started."
+exec /usr/bin/supervisord -c /etc/supervisord.conf
--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -4,6 +4,17 @@ import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            clientPort: 5173,
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true,
+            interval: 1000
+        }
+    },
     plugins: [
         laravel({
             input: ["resources/js/app.js", "resources/css/app.css"],
